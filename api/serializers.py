c267c518cd4775030bdb6acaa59c78584e03e1ef
deniska <dprodan289@gmail.com>
2024-05-13 22:21:41 +0300

    admin role added

--- api/serializers.py
+++ api/serializers.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Permission
 from rest_framework import serializers
 from .models import Comment
 
--- api/views.py
+++ api/views.py
@@ -1,9 +1,13 @@
 from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import UserSerializer, CommentSerializer
-from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, AllowAny, IsAuthenticatedOrReadOnly, BasePermission
 from .models import Comment
 from rest_framework.pagination import PageNumberPagination
+
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        return request.user.is_staff
 
 class CommentListCreate(generics.ListCreateAPIView):
     serializer_class = CommentSerializer
@@ -24,8 +28,12 @@ class CommentDelete(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]
 
     def get_queryset(self):
-        user = self.request.user
-        return Comment.objects.filter(author=user)
+        # Allow admin to delete any comment
+        if IsAdmin().has_permission(self.request, self):
+            return Comment.objects.all()
+        else:
+            # Restrict regular users to delete their own comments
+            return Comment.objects.filter(author=self.request.user)
 
 class CreateUserView(generics.CreateAPIView):
     queryset = User.objects.all()
